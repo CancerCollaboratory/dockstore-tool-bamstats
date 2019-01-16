cf2bc95edad5264dc4b5f16a619155f7de459b4a
NatalieEO <nperez9@ucsc.edu>
2019-01-16 14:58:39 -0800

    Added continuous integration test for updated json file

--- script.sh
+++ script.sh
@@ -7,6 +7,7 @@ set -o xtrace
 
 if [[ "${LANGUAGE}" == "cwl" ]]; then
     cwltool --non-strict Dockstore.cwl test.json
+    cwltool --non-strict Dockstore.cwl sample_configs.local.json
 elif [[ "${LANGUAGE}" == "wdl" ]]; then
     java -jar cromwell-32.jar run Dockstore.wdl --inputs test.wdl.json
 elif [[ "${LANGUAGE}" == "nfl" ]]; then
