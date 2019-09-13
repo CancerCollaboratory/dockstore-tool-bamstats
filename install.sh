9f0d89b5078f809949a4827b1558a3e554ae356f
Denis Yuen <denis.yuen@oicr.on.ca>
2019-09-13 10:35:28 -0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -8,7 +8,7 @@ set -o xtrace
 if [[ "${LANGUAGE}" == "cwl" ]]; then
     pip2.7 install --user cwl-runner cwltool==1.0.20160712154127 schema-salad==1.14.20160708181155 avro==1.8.1
 elif [[ "${LANGUAGE}" == "wdl" ]]; then
-    wget https://github.com/broadinstitute/cromwell/releases/download/32/cromwell-32.jar
+    wget https://github.com/broadinstitute/cromwell/releases/download/44/cromwell-44.jar
 elif [[ "${LANGUAGE}" == "nfl" ]]; then
     curl -s https://get.nextflow.io | bash
     mv nextflow $HOME/bin
