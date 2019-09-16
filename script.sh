f931b70367e625aed6a5333f65677ee2667006d0
Louise Cabansay <ldcabansay@gmail.com>
2019-09-16 15:10:35 -0700

    remove test with ftp download of NA12878 chr20

--- script.sh
+++ script.sh
@@ -7,9 +7,6 @@ set -o xtrace
 
 if [[ "${LANGUAGE}" == "cwl" ]]; then
     cwltool --non-strict Dockstore.cwl test.json
-    wget ftp://ftp.1000genomes.ebi.ac.uk/vol1/ftp/phase3/data/NA12878/alignment/NA12878.chrom20.ILLUMINA.bwa.CEU.low_coverage.20121211.bam
-    mv NA12878.chrom20.ILLUMINA.bwa.CEU.low_coverage.20121211.bam /tmp/
-    cwltool --non-strict Dockstore.cwl sample_configs.local.json
 elif [[ "${LANGUAGE}" == "wdl" ]]; then
     java -jar cromwell-44.jar run Dockstore.wdl --inputs test.wdl.json
 elif [[ "${LANGUAGE}" == "nfl" ]]; then
