0aa1a2c803e072c1eb2f190ac32667bafaa21509
Louise Cabansay <ldcabansay@gmail.com>
2019-09-16 14:00:58 -0700

    pip3.6

--- install.sh
+++ install.sh
@@ -7,7 +7,7 @@ set -o xtrace
 
 if [[ "${LANGUAGE}" == "cwl" ]]; then
     curl -o requirements.txt "https://staging.dockstore.org/api/metadata/runner_dependencies?client_version=1.7.0-rc.2&python_version=3"
-    pip3 install --user -r requirements.txt
+    pip3.6 install --user -r requirements.txt
 elif [[ "${LANGUAGE}" == "wdl" ]]; then
     wget https://github.com/broadinstitute/cromwell/releases/download/44/cromwell-44.jar
 elif [[ "${LANGUAGE}" == "nfl" ]]; then
