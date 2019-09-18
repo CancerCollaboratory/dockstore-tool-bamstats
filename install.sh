d6efbe42c3877ea49d5d582dce96a4062651e656
Andrew Duncan <andrew.duncan@oicr.on.ca>
2019-09-18 16:58:15 +0200

    Feature/2862/update readme and sample inputs (#10)
    * update readme and cwltool/schema-salad versions
    
    * update descriptors to point to newest docker image, also update sample config

--- install.sh
+++ install.sh
@@ -6,7 +6,7 @@ set -o nounset
 set -o xtrace
 
 if [[ "${LANGUAGE}" == "cwl" ]]; then
-    curl -o requirements.txt "https://staging.dockstore.org/api/metadata/runner_dependencies?client_version=1.7.0-rc.2&python_version=3"
+    curl -o requirements.txt "https://raw.githubusercontent.com/dockstore/dockstore/1.7.0-rc.3/dockstore-webservice/src/main/resources/requirements/1.7.0/requirements3.txt"
     pip3 install --user -r requirements.txt
 elif [[ "${LANGUAGE}" == "wdl" ]]; then
     wget https://github.com/broadinstitute/cromwell/releases/download/44/cromwell-44.jar
