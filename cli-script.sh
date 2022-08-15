e69c3ab388633d7ba56fca4a14cb639d71a1d523
Denis Yuen <denis.yuen@oicr.on.ca>
2022-08-15 17:27:59 -0400

    ugh paths

--- cli-script.sh
+++ cli-script.sh
@@ -11,7 +11,6 @@ if [[ "${LANGUAGE}" == "cwl" ]]; then
     jq '.bam_input.path |= "ftp://ftp.1000genomes.ebi.ac.uk/vol1/ftp/phase3/data/NA12878/alignment/NA12878.chrom20.ILLUMINA.bwa.CEU.low_coverage.20121211.bam"| .bamstats_report.path |= "/tmp/bamstats_report.zip"' Dockstore.json | sponge  Dockstore.json
     dockstore workflow launch --entry github.com/dockstore/dockstore-tool-bamstats/bamstats_cwl:feature/update --json Dockstore.json
 elif [[ "${LANGUAGE}" == "wdl" ]]; then
-    printf "cromwell-vm-options: -DLOG_LEVEL=ERROR\nserver-url: https://dockstore.org/api\n" > ~/.dockstore/config	
     dockstore workflow wdl --entry github.com/dockstore/dockstore-tool-bamstats/wdl:feature/update > bamstats.wdl
     dockstore workflow convert wdl2json --wdl bamstats.wdl > Dockstore.json
     dockstore workflow launch --entry github.com/dockstore/dockstore-tool-bamstats/wdl:feature/update --json Dockstore.json
